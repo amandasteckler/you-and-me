ca148fc330488d34189f1490dd791ad3b51bbae5
Flatiron School <flatironschool@Drew-2.local>
2016-12-05 15:42:27 -0500

    Working on Profile. Passing the mic to amanda

--- react-folder/src/components/Profile.js
+++ react-folder/src/components/Profile.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import {Link} from 'react-router'
 // import auth from '../../lib/auth'
 
 class Profile extends Component {
 
-  render(){
-    debugger;
+  handleOnClick(event) {
+    debugger
+    event.preventDefault();
+    this.props.boardRequest(1);
+  }
 
+  render(){
+    debugger
     let boards = this.props.profile.boards.map((board) => {
-      return <li>{board.title}</li>
+      return BOOGERS
     })
 
     return(
@@ -17,7 +23,10 @@ class Profile extends Component {
         <h1>Welcome, {this.props.profile.user_name}.</h1>
         <h2>Your Boards:</h2>
         <div>
-          <ul>{boards}</ul>
+          <ul>
+            {boards}
+            <li><Link to={url}>THIS IS A FUCKING LINK</Link></li>
+          </ul>
         </div>
       </div>
     )
