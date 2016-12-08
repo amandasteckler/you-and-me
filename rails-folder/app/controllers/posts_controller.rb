65be0da9c37adc51b93f3d69c149beac8ff9059f
Flatiron School <flatironschool@Drew-2.local>
2016-12-07 21:28:35 -0500

    new OrderPosts object to....order the posts

--- rails-folder/app/controllers/posts_controller.rb
+++ rails-folder/app/controllers/posts_controller.rb
@@ -9,11 +9,7 @@ class PostsController < ApplicationController
     post = Post.new(post_params)
     if post.save
       board = Board.find(UserBoard.find(post_params[:user_board_id]).board.id)
-      rawPosts = board.user_boards.map {|user_board| user_board.posts}.flatten
-      # [{Post}, {}, {}]
-      order_posts = rawPosts.sort_by {|post| post.created_at}.reverse
-      orderedWithUser = order_posts.map {|post| {id: post.id, content: post.content, userID: post.user_board.user.id, userName: post.user_board.user.name}}
-
+      orderedWithUser = OrderedPosts.new.sort_with_user(board)
       render json: {posts: orderedWithUser}
     else
       render json: {errors: "post not succesful"}
@@ -34,20 +30,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    post = Post.find(post_params[:id])
     board = Board.find(post_params[:board_id])
-
     Post.delete(params[:id])
-
-    rawPosts = board.user_boards.map {|user_board| user_board.posts}.flatten
-    # [{Post}, {}, {}]
-    order_posts = rawPosts.sort_by {|post| post.created_at}.reverse
-    orderedWithUser = order_posts.map {|post| {id: post.id, content: post.content, userID: post.user_board.user.id, userName: post.user_board.user.name}}
-
-
-
+    orderedWithUser = OrderedPosts.new.sort_with_user(board)
     render json: {posts: orderedWithUser}
-
   end
 
   private
---
+++ rails-folder/lib/ordered_posts.rb
@@ -0,0 +1,10 @@
+class OrderedPosts
+
+  def sort_with_user(board)
+    rawPosts = board.user_boards.map {|user_board| user_board.posts}.flatten
+    # [{Post}, {}, {}]
+    order_posts = rawPosts.sort_by {|post| post.created_at}.reverse
+    orderedWithUser = order_posts.map {|post| {id: post.id, content: post.content, userID: post.user_board.user.id, userName: post.user_board.user.name}}
+  end
+
+end
