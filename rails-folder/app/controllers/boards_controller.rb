ded1a0474d26e51aff9db1bd9fc1be156bfc7d8a
Flatiron School <flatironschool@Drew-2.local>
2016-12-15 09:29:06 -0500

    Fixed posts being persisted between boards

--- rails-folder/app/controllers/boards_controller.rb
+++ rails-folder/app/controllers/boards_controller.rb
@@ -30,7 +30,6 @@ class BoardsController < ApplicationController
     posts = OrderedPosts.new.sort_with_user(board)
     images = OrderedImages.new.sort_with_user(board)
 
-
     render json: {board: {id: board.id, title: board.title, user_boards: board.user_boards, users: users, posts: posts, images: images}}
 
   end
